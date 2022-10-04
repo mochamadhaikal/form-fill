b5267a08280252664ba3a1839bbd2f00945483e2
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2022-10-04 15:42:31 +0700

    fix download

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -8,13 +8,23 @@ const Router = express.Router();
 
 Router.get('/users',async function(req, res, next) {
    try {
-      res.download('./pdf/coba1.pdf').send('success')
+      res.send(users)
+      // res.download('./pdf/output.pdf', 'form.pdf')
    } catch (error) {
       console.log(error);
    }
 })
 
 Router.post('/users', async function(req, res, next) {
+   users.push(req.body)
+   res.status(200).send("success")
+})
+
+Router.get('/users/:id', async function(req, res, next) {
+   const id = req.params.id
+   const data = users.filter((user) => {
+      return user.id == id
+   })
    const pdfDoc = await PDFDocument.load(await readFile(`${__dirname}/../pdf/form.pdf`))
 
    pdfDoc.removePage(0)
@@ -23,7 +33,7 @@ Router.post('/users', async function(req, res, next) {
 
    const form = pdfDoc.getForm()
 
-   req.body.data.aof.map((data, i) => {
+   data[0].data.aof.map((data, i) => {
       //  AVA Asian Opportunities Fund
       form.getTextField(`aof_tgl_transaksi_${i}`).setText(data.tgl_transaksi)
       form.getTextField(`aof_jenis_transaksi_${i}`).setText(data.jenis_transaksi)
@@ -33,7 +43,7 @@ Router.post('/users', async function(req, res, next) {
       form.getTextField(`aof_total_akumulasi_unit_${i}`).setText(`(${data.akumulasi_unit})`)
    })
 
-   req.body.data.fipf.map((data, i) => {
+   data[0].data.fipf.map((data, i) => {
       // AVA Fixed Income Plus Fund
       form.getTextField(`fipf_tgl_transaksi_${i}`).setText(data.tgl_transaksi)
       form.getTextField(`fipf_jenis_transaksi_${i}`).setText(data.jenis_transaksi)
@@ -43,7 +53,7 @@ Router.post('/users', async function(req, res, next) {
       form.getTextField(`fipf_total_akumulasi_unit_${i}`).setText(`(${data.akumulasi_unit})`)
    })
 
-   req.body.data.gpf.map((data, i) => {
+   data[0].data.gpf.map((data, i) => {
       //  AVA Growth Plus Fund
       form.getTextField(`gpf_tgl_transaksi_${i}`).setText(data.tgl_transaksi)
       form.getTextField(`gpf_jenis_transaksi_${i}`).setText(data.jenis_transaksi)
@@ -53,7 +63,7 @@ Router.post('/users', async function(req, res, next) {
       form.getTextField(`gpf_total_akumulasi_unit_${i}`).setText(`(${data.akumulasi_unit})`)
    })
 
-   req.body.data.scef.map((data, i) => {
+   data[0].data.scef.map((data, i) => {
       //  AVA Small Cap Equity Fund
       form.getTextField(`scef_tgl_transaksi_${i}`).setText(data.tgl_transaksi)
       form.getTextField(`scef_jenis_transaksi_${i}`).setText(data.jenis_transaksi)
@@ -63,7 +73,7 @@ Router.post('/users', async function(req, res, next) {
       form.getTextField(`scef_total_akumulasi_unit_${i}`).setText(`(${data.akumulasi_unit})`)
    })
 
-   req.body.data.sf.map((data, i) => {
+   data[0].data.sf.map((data, i) => {
       //  AVA Secure Fund
       form.getTextField(`sf_tgl_transaksi_${i}`).setText(data.tgl_transaksi)
       form.getTextField(`sf_jenis_transaksi_${i}`).setText(data.jenis_transaksi)
@@ -73,7 +83,7 @@ Router.post('/users', async function(req, res, next) {
       form.getTextField(`sf_total_akumulasi_unit_${i}`).setText(`(${data.akumulasi_unit})`)
    })
 
-   const ringkasan = req.body.ringkasan;
+   const ringkasan = data[0].ringkasan;
 
       form.getTextField(`aof_saldo_akhir`).setText(`${ringkasan.aof_saldo_akhir}`)
       form.getTextField(`aof_harga_unit`).setText(`${ringkasan.aof_harga_unit}`)
@@ -89,7 +99,7 @@ Router.post('/users', async function(req, res, next) {
       form.getTextField(`scef_nilai_dana`).setText(`${ringkasan.scef_nilai_dana}`)
       form.getTextField(`sf_saldo_akhir`).setText(`${ringkasan.sf_saldo_akhir}`)
       form.getTextField(`sf_harga_unit`).setText(`${ringkasan.sf_harga_unit}`)
-      form.getTextField(`sf_nilai_dana`).setText(`${ringkasan.sf_nilai_dana}`)
+      // form.getTextField(`sf_nilai_dana`).setText(`${ringkasan.sf_nilai_dana}`)
 
       form.getTextField(`total_dana`).setText(`
       ${
@@ -104,12 +114,9 @@ Router.post('/users', async function(req, res, next) {
 
    const pdfBtyes = await pdfDoc.save()
    await writeFile(`${__dirname}/../pdf/output.pdf`, pdfBtyes)
+   // console.log(data.id);
 
-   users.push(req.body)
-   res.status(200).send({
-      "success": true,
-      "filepath": "users/:id/output.pdf"
-  })
+   res.status(200).download(`${__dirname}/../pdf/output.pdf`, 'form.pdf')
 })
 
 module.exports = Router;
